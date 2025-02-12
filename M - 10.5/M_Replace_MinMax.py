4d73a39a0df8fa6e398025ccc38dc583ff579759
Asir Intisar <aintisar48@gmail.com>
2025-02-12 16:57:16 +0600

    [M-10.5] - problem 2

---
+++ M - 10.5/M_Replace_MinMax.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+fmin = min(arr)
+fmax = max(arr)
+
+min_indx = arr.index(fmin)
+max_indx = arr.index(fmax)
+
+
+tmp = arr[min_indx]
+arr[min_indx] = arr[max_indx]
+arr[max_indx] = tmp
+
+print(*arr)
